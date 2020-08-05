5519b9e27bbbbd9a59b68ec915ec4daf8cb7a536
Victor Magalhães <victormagalhaes00@outlook.com>
2020-08-05 20:55:50 -0300

    Adicionando função para ler do arquivo

--- src/relacao.c
+++ src/relacao.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define TAMANHO_MAXIMO_ARQUIVO 1000
 
 typedef struct
 {
@@ -6,8 +8,31 @@ typedef struct
     int y;
 } ParOrdenado;
 
+char *leDoArquivo()
+{
+    static char arquivo[TAMANHO_MAXIMO_ARQUIVO];
+    FILE *ponteiroArquivo;
+    if ((ponteiroArquivo = fopen("input.txt", "r")) == NULL)
+    {
+        printf("Arquivo não encontrado, cheque se o arquivo input.txt está na mesma pasta que o executável!");
+        exit(1);
+    }
+    fscanf(ponteiroArquivo, "%[^EOF]", arquivo);
+    int tamanhoArquivo = ftell(ponteiroArquivo);
+    arquivo[tamanhoArquivo] = EOF;
+    fclose(ponteiroArquivo);
+    return arquivo;
+}
+
 int main()
 {
+    char *arquivo;
+    arquivo = leDoArquivo();
+    while (*arquivo != EOF)
+    {
+        printf("%c", *arquivo);
+        arquivo++;
+    }
 
     return 0;
 }
