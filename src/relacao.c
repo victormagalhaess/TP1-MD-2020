7aacee248ebbf4cec03bad8e00ce35b2fb37e050
Victor Hugo <victormagalhaes00@outlook.com>
2020-08-15 14:43:07 -0300

    Feature/ler string do arquivo (#2)
    * Adicionando função para ler do arquivo
    
    * Finalizando leitura dos pares ordenados

--- src/relacao.c
+++ src/relacao.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define TAMANHO_MAXIMO_ARQUIVO 1000
 
 typedef struct
 {
@@ -8,6 +10,39 @@ typedef struct
 
 int main()
 {
+
+    FILE *ponteiroArquivo;
+    if ((ponteiroArquivo = fopen("input.txt", "r")) == NULL)
+    {
+        printf("Arquivo não encontrado, cheque se o arquivo input.txt está na mesma pasta que o executável!");
+        exit(1);
+    }
+    int quantidadeDeRelacoes = 0;
+    ParOrdenado relacoes[TAMANHO_MAXIMO_ARQUIVO];
+    int numeroDeElementos;
+    fscanf(ponteiroArquivo, "%d", &numeroDeElementos);
+    int elementos[numeroDeElementos];
+
+    for (int i = 0; i < numeroDeElementos; i++)
+    {
+        fscanf(ponteiroArquivo, "%d", &elementos[i]);
+    }
+
+    while (!feof(ponteiroArquivo) && quantidadeDeRelacoes < TAMANHO_MAXIMO_ARQUIVO)
+    {
+        fscanf(ponteiroArquivo, "%d %d", &relacoes[quantidadeDeRelacoes].x, &relacoes[quantidadeDeRelacoes].y);
+        quantidadeDeRelacoes++;
+    }
+
+    for (int i = 0; i < numeroDeElementos; i++)
+    {
+        printf("%d\n", elementos[i]);
+    }
+
+    for (int i = 0; i < quantidadeDeRelacoes; i++)
+    {
+        printf("x: %d y: %d \n", relacoes[i].x, relacoes[i].y);
+    }
 
     return 0;
 }
