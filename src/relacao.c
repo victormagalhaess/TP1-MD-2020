1f26691c82c12e1936acf75fbf23be45e022b9cc
Victor Magalhães <victormagalhaes00@outlook.com>
2020-08-15 17:20:31 -0300

    relacoes reflexivas, irreflexivas, simetricas, antissimetricas e assimetricas

--- src/relacao.c
+++ src/relacao.c
@@ -53,12 +53,183 @@ int main()
         matrizDeRelacoes[origem][destino] = 1;
     }
 
+    //VERIFICAR PROPRIEDADES
+    printf("Propriedades:\n");
+
+    //VERIFICAR SE É REFLEXIVA
+    Elemento faltaParaSerReflexiva[numeroDeElementos];
+    int EhReflexiva = 1;
+    int numeroDeFaltamParaSerReflexiva = 0;
     for (int i = 0; i < numeroDeElementos; i++)
     {
+        if (!(matrizDeRelacoes[i][i]))
+        {
+            EhReflexiva = 0;
+            faltaParaSerReflexiva[numeroDeFaltamParaSerReflexiva].x = elementos[i];
+            faltaParaSerReflexiva[numeroDeFaltamParaSerReflexiva].y = elementos[i];
+            numeroDeFaltamParaSerReflexiva++;
+        }
+    }
+    printf("1. Reflexiva: ");
+    if (EhReflexiva)
+    {
+        printf("V\n");
+    }
+    else
+    {
+        printf("F\n   ");
+        for (int i = 0; i < numeroDeFaltamParaSerReflexiva; i++)
+        {
+            printf("(%d, %d); ", faltaParaSerReflexiva[i].x, faltaParaSerReflexiva[i].y);
+        }
+        printf("\n");
+    }
 
+    //VERIFICAR SE É IRREFLEXIVA
+    Elemento faltaParaSerIrreflexiva[numeroDeElementos];
+    int EhIrreflexiva = 1;
+    int numeroDeFaltamParaSerIrreflexiva = 0;
+    for (int i = 0; i < numeroDeElementos; i++)
+    {
+        if ((matrizDeRelacoes[i][i]))
+        {
+            EhIrreflexiva = 0;
+            faltaParaSerIrreflexiva[numeroDeFaltamParaSerIrreflexiva].x = elementos[i];
+            faltaParaSerIrreflexiva[numeroDeFaltamParaSerIrreflexiva].y = elementos[i];
+            numeroDeFaltamParaSerIrreflexiva++;
+        }
+    }
+    printf("2. Irreflexiva: ");
+    if (EhIrreflexiva)
+    {
+        printf("V\n");
+    }
+    else
+    {
+        printf("F\n   ");
+        for (int i = 0; i < numeroDeFaltamParaSerIrreflexiva; i++)
+        {
+            printf("(%d, %d); ", faltaParaSerIrreflexiva[i].x, faltaParaSerIrreflexiva[i].y);
+        }
+        printf("\n");
+    }
+
+    //VERIFICA SE É SIMÉTRICA
+    int EhSimetrica = 1;
+    Elemento faltaParaSerSimetrica[quantidadeDeRelacoes];
+    int numeroDeFaltamParaSerSimetrica = 0;
+    for (int i = 0; i < numeroDeElementos; i++)
+    {
         for (int j = 0; j < numeroDeElementos; j++)
         {
-            printf("%d  ", matrizDeRelacoes[i][j]);
+            if (matrizDeRelacoes[i][j])
+            {
+                if (!matrizDeRelacoes[j][i])
+                {
+                    EhSimetrica = 0;
+                    faltaParaSerSimetrica[numeroDeFaltamParaSerSimetrica].x = elementos[j];
+                    faltaParaSerSimetrica[numeroDeFaltamParaSerSimetrica].y = elementos[i];
+                    numeroDeFaltamParaSerSimetrica++;
+                }
+            }
+        }
+    }
+
+    printf("3. Simétrica: ");
+    if (EhSimetrica)
+    {
+        printf("V\n");
+    }
+    else
+    {
+        printf("F\n   ");
+        for (int i = 0; i < numeroDeFaltamParaSerSimetrica; i++)
+        {
+            printf("(%d, %d); ", faltaParaSerSimetrica[i].x, faltaParaSerSimetrica[i].y);
+        }
+        printf("\n");
+    }
+
+    //VERIFICA SE É ANTI-SIMÉTRICA
+    int EhAntisimetrica = 1;
+    Elemento faltaParaSerAntisimetrica[quantidadeDeRelacoes];
+    int numeroDeFaltamParaSerAntisimetrica = 0;
+    int elementoJaContado = 0;
+    for (int i = 0; i < numeroDeElementos; i++)
+    {
+        for (int j = 0; j < numeroDeElementos; j++)
+        {
+            if (matrizDeRelacoes[i][j] && i != j)
+            {
+                if (matrizDeRelacoes[j][i])
+                {
+                    for (int k = 0; k < numeroDeFaltamParaSerAntisimetrica; k++)
+                    {
+                        if (faltaParaSerAntisimetrica[k].y == elementos[j] && faltaParaSerAntisimetrica[k].x == elementos[i])
+                        {
+                            elementoJaContado = 1;
+                        }
+                    }
+                    if (!elementoJaContado)
+                    {
+                        EhAntisimetrica = 0;
+                        faltaParaSerAntisimetrica[numeroDeFaltamParaSerAntisimetrica].x = elementos[j];
+                        faltaParaSerAntisimetrica[numeroDeFaltamParaSerAntisimetrica].y = elementos[i];
+                        numeroDeFaltamParaSerAntisimetrica++;
+                    }
+                    elementoJaContado = 0;
+                }
+            }
+        }
+    }
+
+    printf("4. Anti-simétrica: ");
+    if (EhAntisimetrica)
+    {
+        printf("V\n");
+    }
+    else
+    {
+        printf("F\n   ");
+        for (int i = 0; i < numeroDeFaltamParaSerAntisimetrica; i++)
+        {
+            printf("(%d, %d) e (%d, %d); ", faltaParaSerAntisimetrica[i].x, faltaParaSerAntisimetrica[i].y, faltaParaSerAntisimetrica[i].y, faltaParaSerAntisimetrica[i].x);
+        }
+        printf("\n");
+    }
+
+    //VERIFICA SE É ASSIMÉTRICA
+    int EhAssimetrica = 1;
+    Elemento faltaParaSerAssimetrica[quantidadeDeRelacoes];
+    int numeroDeFaltamParaSerAssimetrica = 0;
+    for (int i = 0; i < numeroDeElementos; i++)
+    {
+        for (int j = 0; j < numeroDeElementos; j++)
+        {
+            if (matrizDeRelacoes[i][j])
+            {
+                if (matrizDeRelacoes[j][i])
+                {
+                    EhAssimetrica = 0;
+                    faltaParaSerAssimetrica[numeroDeFaltamParaSerAssimetrica].x = elementos[j];
+                    faltaParaSerAssimetrica[numeroDeFaltamParaSerAssimetrica].y = elementos[i];
+                    numeroDeFaltamParaSerAssimetrica++;
+                }
+            }
+        }
+    }
+
+    printf("5. Assimétrica: ");
+    if (EhAssimetrica)
+    {
+        printf("V\n");
+    }
+    else
+    {
+        printf("F\n   ");
+        for (int i = 0; i < numeroDeFaltamParaSerAssimetrica; i++)
+        {
+            printf("(%d, %d); ", faltaParaSerAssimetrica[i].x, faltaParaSerAssimetrica[i].y);
         }
         printf("\n");
     }
