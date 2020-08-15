b2a8a2cc7f6381af440a69f36bb7433dcc80f51c
Victor Magalhães <victormagalhaes00@outlook.com>
2020-08-15 14:36:09 -0300

    Finalizando leitura dos pares ordenados

--- src/relacao.c
+++ src/relacao.c
@@ -8,30 +8,40 @@ typedef struct
     int y;
 } ParOrdenado;
 
-char *leDoArquivo()
+int main()
 {
-    static char arquivo[TAMANHO_MAXIMO_ARQUIVO];
+
     FILE *ponteiroArquivo;
     if ((ponteiroArquivo = fopen("input.txt", "r")) == NULL)
     {
         printf("Arquivo não encontrado, cheque se o arquivo input.txt está na mesma pasta que o executável!");
         exit(1);
     }
-    fscanf(ponteiroArquivo, "%[^EOF]", arquivo);
-    int tamanhoArquivo = ftell(ponteiroArquivo);
-    arquivo[tamanhoArquivo] = EOF;
-    fclose(ponteiroArquivo);
-    return arquivo;
-}
+    int quantidadeDeRelacoes = 0;
+    ParOrdenado relacoes[TAMANHO_MAXIMO_ARQUIVO];
+    int numeroDeElementos;
+    fscanf(ponteiroArquivo, "%d", &numeroDeElementos);
+    int elementos[numeroDeElementos];
 
-int main()
-{
-    char *arquivo;
-    arquivo = leDoArquivo();
-    while (*arquivo != EOF)
+    for (int i = 0; i < numeroDeElementos; i++)
     {
-        printf("%c", *arquivo);
-        arquivo++;
+        fscanf(ponteiroArquivo, "%d", &elementos[i]);
+    }
+
+    while (!feof(ponteiroArquivo) && quantidadeDeRelacoes < TAMANHO_MAXIMO_ARQUIVO)
+    {
+        fscanf(ponteiroArquivo, "%d %d", &relacoes[quantidadeDeRelacoes].x, &relacoes[quantidadeDeRelacoes].y);
+        quantidadeDeRelacoes++;
+    }
+
+    for (int i = 0; i < numeroDeElementos; i++)
+    {
+        printf("%d\n", elementos[i]);
+    }
+
+    for (int i = 0; i < quantidadeDeRelacoes; i++)
+    {
+        printf("x: %d y: %d \n", relacoes[i].x, relacoes[i].y);
     }
 
     return 0;
